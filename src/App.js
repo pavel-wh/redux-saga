51ee4cd8b631d2236d32bc89620d72f37aab14b3
pB <webforeman@gmail.com>
2020-03-27 18:02:59 +0300

    Check bug with quotes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import FetchedPosts from './components/FetchedPosts'
 
 function App() {
   return (
-		<div className=container pt-3>
-			<div className=row>
-				<div className=col>
+		<div className="container pt-3">
+			<div className="row">
+				<div className="col">
           <PostForm />
         </div>
 			</div>
-			<div className=row>
-				<div className=col>
+			<div className="row">
+				<div className="col">
           <h2>Синхронные посты</h2>
           <Posts />
         </div>
-				<div className=col>
+				<div className="col">
           <h2>Acинхронные посты</h2>
           <FetchedPosts />
         </div>
